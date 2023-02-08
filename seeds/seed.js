659e73d0ab88824c16b677385550d4507ac9214b
Olubukola Abiona <bukyabiona@gmail.com>
2023-02-08 18:37:34 -0400

    created seeds js file in seeds folder

---
+++ seeds/seed.js
@@ -0,0 +1,26 @@
+const sequelize = require('../config/connection');
+const { User, Project } = require('../models');
+
+const userdata = require('./userData.json');
+const blogdata = require('./blogData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({force: true});
+
+    const users = await User.bulkCreate(userdata, {
+        individualHooks: true,
+        returning: true
+    });
+
+    for (const blog of blogdata) {
+        await Blog.create({
+            ...blog,
+            user_id: users[Math.floor(Math.random() * users.length)].id
+        });
+    }
+
+    process.exit(0);
+};
+
+seedDatabase();
+
