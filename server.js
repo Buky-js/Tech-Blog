395e2041ed3f1664f483dec2cac9446df8b2af18
Olubukola Abiona <bukyabiona@gmail.com>
2023-02-08 18:17:49 -0400

    created more js files for the project

--- server.js
+++ server.js
@@ -2,13 +2,39 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const PORT = 3001;
-
+const session = require('express-session');
+const sequelize = require('./controllers');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
 app.set('view engine', 'handlebars');
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}.` )
+const sess = {
+    secret: 'very coded secret',
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
+
+sequelize.sync({
+    force: false
+}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}.`)
+    })
 })
