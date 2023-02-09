c5cdfe180ba5f6c21cd82a1ed2cc556f3de294d4
Olubukola Abiona <bukyabiona@gmail.com>
2023-02-09 01:17:09 -0400

    modified the files and handlebars to display the homepage

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const exphbs = require('express-handlebars');
 const PORT = 3001;
 const session = require('express-session');
@@ -7,15 +8,15 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 
-app.set('view engine', 'handlebars');
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 const sess = {
     secret: 'very coded secret',
